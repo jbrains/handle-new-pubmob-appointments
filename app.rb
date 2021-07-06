eb153c0e32894c1a1b6d9dbb29441155a8f1931f
J. B. Rainsberger <me@jbrains.ca>
2021-07-06 19:52:05 -0300

    Updated the Integrately webhook URL, hoping that it works this time.

--- app.rb
+++ app.rb
@@ -9,8 +9,8 @@ class HelloWorldApp < Sinatra::Base
         puts mail.inspect
         booking_details = ExtractFlexbookerBookingDetails.parse(mail)
         if booking_details
-            puts "Post a request to https://app.integrately.com/a/webhooks/92ced5fabafd4a30ab9729b6951d3fd2 with the parts #{booking_details.inspect}"
-            HTTParty.post("https://app.integrately.com/a/webhooks/92ced5fabafd4a30ab9729b6951d3fd2", {body: {customer_email: booking_details.customer_email, customer_full_name: booking_details.customer_full_name}})
+            puts "Post a request to https://app.integrately.com/a/webhooks/e80a5f7584924e4a99d05d0b31dfd632 with the parts #{booking_details.inspect}"
+            HTTParty.post("https://app.integrately.com/a/webhooks/e80a5f7584924e4a99d05d0b31dfd632", {body: {customer_email: booking_details.customer_email, customer_full_name: booking_details.customer_full_name}})
             200
         else
             400
