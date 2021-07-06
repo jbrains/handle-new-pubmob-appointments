2e452416a19f3c525dd15ad7bd2ee4377d8f9727
J. B. Rainsberger <me@jbrains.ca>
2021-07-06 18:25:09 -0300

    We gave up on trying to output information with JSON, because I just can't.

--- app.rb
+++ app.rb
@@ -1,13 +1,12 @@
 require 'sinatra'
-require 'json'
 
 class HelloWorldApp < Sinatra::Base
     post '/echo' do
         mail = params
-        puts mail.to_json
+        puts mail.inspect
         booking_details = ExtractFlexbookerBookingDetails.parse(mail)
         if booking_details
-            puts "Post a request to https://app.integrately.com/a/webhooks/92ced5fabafd4a30ab9729b6951d3fd2 with the parts #{booking_details.to_json}"
+            puts "Post a request to https://app.integrately.com/a/webhooks/92ced5fabafd4a30ab9729b6951d3fd2 with the parts #{booking_details.inspect}"
 
         else
             400
