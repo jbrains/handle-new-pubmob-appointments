c6eaa046fd9bd789b7fbe85e022c987703ce6a51
J. B. Rainsberger <me@jbrains.ca>
2021-07-07 14:30:27 -0300

    Documented a refactoring task.

--- app.rb
+++ app.rb
@@ -10,6 +10,7 @@ class HelloWorldApp < Sinatra::Base
         booking_details = ExtractFlexbookerBookingDetails.parse(mail)
         if booking_details
             puts "Post a request to https://app.integrately.com/a/webhooks/e80a5f7584924e4a99d05d0b31dfd632 with the parts #{booking_details.inspect}"
+            # REFACTOR Extract to an environment variable
             HTTParty.post("https://app.integrately.com/a/webhooks/e80a5f7584924e4a99d05d0b31dfd632", {body: {customer_email: booking_details.customer_email, customer_full_name: booking_details.customer_full_name}})
             200
         else
