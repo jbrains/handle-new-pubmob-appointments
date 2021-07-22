ebf2ee5dcaca08e0be6966b3c7dd53d60e0ca0d1
J. B. Rainsberger <me@jbrains.ca>
2021-07-22 10:53:05 -0300

    Improved formatting.

--- app.rb
+++ app.rb
@@ -6,19 +6,22 @@ require './lib/flexbooker'
 BookingDetailsDestinationEndpointUrl = ENV["DESTINATION_ENDPOINT_URL"]
 
 class HandleNewEvolutionaryDesignWithoutTestsBookingApp < Sinatra::Base
-    post '/booking/email-notification' do
-        return [500, {}, ["I can't find the booking details destination endpoint URL."]] unless BookingDetailsDestinationEndpointUrl
+  post '/booking/email-notification' do
+    return [500, {},
+            ["I can't find the booking details destination endpoint URL."]] unless BookingDetailsDestinationEndpointUrl
 
-        mail = params
-        puts mail.inspect
-        booking_details = ExtractFlexbookerBookingDetails.parse(mail).parse()
-        if booking_details
-            puts "Received a booking email with the parts #{booking_details.inspect}"
-            # REFACTOR Extract to an environment variable
-            HTTParty.post(BookingDetailsDestinationEndpointUrl, {body: {customer_email: booking_details.customer_email, customer_full_name: booking_details.customer_full_name}})
-            200
-        else
-            [400, {}, ["This doesn't look like a Flexbooker email.", mail.inspect]]
-        end
+    mail = params
+    puts mail.inspect
+    booking_details = ExtractFlexbookerBookingDetails.parse(mail).parse()
+    if booking_details
+      puts "Received a booking email with the parts #{booking_details.inspect}"
+      # REFACTOR Extract to an environment variable
+      HTTParty.post(BookingDetailsDestinationEndpointUrl,
+                    { body: { customer_email: booking_details.customer_email,
+                              customer_full_name: booking_details.customer_full_name } })
+      200
+    else
+      [400, {}, ["This doesn't look like a Flexbooker email.", mail.inspect]]
     end
+  end
 end
